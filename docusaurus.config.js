8c896a70966c1cc87dc55470b39c6724de329596
KhaPhan <pttkha97@gmail.com>
2023-12-09 23:56:47 +0700

    update blog and cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Rainee satellite',
-  tagline: 'We are cool',
+  tagline: "Let's explore the state",
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
